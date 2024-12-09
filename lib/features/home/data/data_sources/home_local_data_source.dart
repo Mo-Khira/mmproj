04f763e0cbfdf1bda908b4471e0ca23df66266c3
Mo-Khira <110000761+Mo-Khira@users.noreply.github.com>
2024-12-09 19:25:45 +0200

    add hive to project

---
+++ lib/features/home/data/data_sources/home_local_data_source.dart
@@ -0,0 +1,20 @@
+import 'package:mmproj/features/home/domain/entities/book_entity.dart';
+
+abstract class HomeLocalDataSource {
+  List<BookEntity> fetchFeaturedBooks();
+  List<BookEntity> fetchNewestBooks();
+}
+
+class HomeRemoteDataSourceImpl extends HomeLocalDataSource {
+  @override
+  List<BookEntity> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  List<BookEntity> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+}
